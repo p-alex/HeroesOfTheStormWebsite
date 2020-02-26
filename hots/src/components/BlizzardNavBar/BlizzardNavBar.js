0d09773b405b0ad570ec57e31e35beced8ea38e1
p-alex <alexey9021@gmail.com>
2020-02-26 13:57:12 +0200

    images

---
+++ hots/src/components/BlizzardNavBar/BlizzardNavBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class BlizzardNavBar extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="blizz-nav">
+        <div className="blizz-nav-content">
+          <img src="/images/blizz-logo.png" alt="blizz" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BlizzardNavBar;
