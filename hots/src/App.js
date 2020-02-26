09a57fd204edfe9602ce6eeff849c7843c967268
p-alex <alexey9021@gmail.com>
2020-02-26 13:38:57 +0200

    Initial commit

---
+++ hots/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import HomePage from "./containers/Homepage/Homepage";
+
+function App() {
+  return (
+    <React.Fragment>
+      <HomePage />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ hots/src/containers/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BlizzardNavBar from "../../components/BlizzardNavBar/BlizzardNavBar";
+
+const homePage = props => {
+  return (
+    <React.Fragment>
+      <BlizzardNavBar />
+    </React.Fragment>
+  );
+};
+
+export default homePage;
