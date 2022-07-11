67ca0a6f86e6f680756fb04aa63a290515fa4dd7
Alex Daniel <alexey9021@gmail.com>
2022-07-11 23:08:23 +0300

    redeploy

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.listen(process.env.PORT || 8000, () => {
   console.log("Server started on port 8000");
 });
 
+console.log("redeploy");
+
 //Use Routes
 
 app.use("/api/champions", champions);
